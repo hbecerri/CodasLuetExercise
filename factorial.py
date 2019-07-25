7d2aaa785128840ee5e7bffafad1ee945da54a8d
Hugo <hbecer2@uic.edu>
2019-07-25 16:11:39 -0400

    Trouble

--- factorial.py
+++ factorial.py
@@ -5,7 +5,7 @@ def recur_factorial(n):
     """Function to return the factorial
     of a number using recursion"""
     if n == 1:
-        return n
+        return n;
     else:
         return n*recur_factorial(n-1)
 
